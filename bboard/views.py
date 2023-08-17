401f64a23cd9c160cfe7bb67f0d6ee7b3a4e81bc
NewJobHunter <euromen@yandex.ru>
2023-08-17 17:50:50 +0300

    Исправленна ошибка создания приложения, созданна Db, добавленны две записи

--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 
+from bboard.models import Bb
+
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    s = "Список объявлений\r\n\r\n\r\n"
+    for bb in Bb.objects.order_by('-published'):
+        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
